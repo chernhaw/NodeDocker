d84778eeba2a6c4f2dcc0d33bc6f2e4245c2fa36
chernhaw <38425578+chernhaw@users.noreply.github.com>
2023-02-27 11:01:48 +0800

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(3000);
+const port = 3000;
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+res.send('<h1>Hello<h1>')
+})
+
+app.get('/date', function(req, res){
+  let date_ob = new Date()
+  res.send('Today is <br><h2>' + date_ob+'</h2>')
+  })
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
